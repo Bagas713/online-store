4bb6b820d0a6f889649f559ae5d71f00947ed5d1
Bagas <bagasgumelar17@gmail.com>
2023-06-09 22:15:53 +0700

    Online Store MVC

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Product;
+
+class ProductSeeder extends Seeder
+{
+    //how to create dummy data product?
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+    }
+}
